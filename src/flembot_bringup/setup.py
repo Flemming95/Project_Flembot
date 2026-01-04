59789ea0a0761315829d465bdf714fe74cdf7fdc
Flemming <flemmingmobile@gmail.com>
2026-01-04 14:29:37 +0000

    Updated

--- src/flembot_bringup/setup.py
+++ src/flembot_bringup/setup.py
@@ -7,15 +7,19 @@ package_name = 'flembot_bringup'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
             glob('launch/*.py')),
+        # FIX: include .world files (not ".worlds")
         (os.path.join('share', package_name, 'worlds'),
-            glob('worlds/*.worlds')),
+            glob('worlds/*.world')),
+        # include config files so installed package has the ros_gz_config.yaml
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
